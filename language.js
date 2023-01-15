9d4b934a1310e243b9025bd7b369c91e96ad8e8e
ATABESHA <121684369+ATABESHA@users.noreply.github.com>
2023-01-15 14:27:53 +0100

    Add files via upload

---
+++ language.js
@@ -0,0 +1,15 @@
+function lang_to_EN(){
+    window.open("indexEN.html","_self");
+}
+
+function lang_to_FR(){
+    window.open("indexFR.html","_self");
+}
+
+function lang_to_ES(){
+    window.open("index.html","_self");
+}
+
+function lang_to_CAT(){
+    window.open("indexCAT.html","_self");
+}
